3611724db49bfa18f58e0bf507f067edc049d918
rad <cepelka.jr@email.cz>
2021-02-18 16:59:50 +0100

    KNN modules

---
+++ KNN_iris.py
@@ -0,0 +1,6 @@
+from sklearn import neighbors # Samotný KNN algoritmus
+from sklearn import preprocess # Normalizujeme data
+from sklearn.model_selection  import train_test_split # Rozdělíme data na "trénovací" a testovací 80% ku 20%
+import numpy as np
+from sklearn.datasets import load_iris # Funkce pro načtění datasetu
+

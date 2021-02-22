5cf6d32b350f9272a91eaacdb1d41ba6d54cd614
rad <cepelka.jr@email.cz>
2021-02-22 15:42:04 +0100

    class KNN

--- KNN_demo.py
+++ KNN_demo.py
@@ -6,24 +6,41 @@ from sklearn.model_selection  import train_test_split
 
 
 class KNN():
-
     def __init__(self, dataset_x, dataset_y, k):
-        assert len(dataset_x) == len(dataset_y) # Počet vzorků se shoduje
-        assert len(dataset_x) >= k # K není větší než počet vzorků
-        self.dataset = np.array( list( zip(X, Y) ) )
+        assert len(dataset_x) == len(dataset_y)  # Počet vzorků se shoduje
+        assert len(dataset_x) >= k  # K není větší než počet vzorků
+        self.dataset = np.array(list(zip(X, Y)))
         self.k = k
 
-    def predict(self):
-        pass
+    def distance(self, a, b):
+        return np.sqrt(np.sum((a - b) ** 2))  # Euklidovská vzdálenost
+    # zatím bez váhy jednotlivých sousedů
+    # váha je 1 / vzdálenost
+
+    def predict(self, x):
+        nearest_points = sorted(self.dataset, key=lambda _x: self.distance(x, _x[0]))[:self.k]
+        classes = list(zip(*nearest_points))[1]
+        return Counter(classes).most_common(1)[0][0]
+
+    def score(self, x, y):
+        right = 0
+        total = 0
+        for _x, _y in zip(x, y):
+            pred = self.predict(_x)
+            if pred == _y:
+                right += 1
+            total += 1
+        return right / total
 
 # vytvoření demo datasetu
+# vlastnosti = souřadnice x, y
 X = [
     [2, 3],
     [5, 6],
     [8, 2],
     [6, 3]
     ]
-
+# label = barva
 Y = [
     2,
     2,
@@ -50,3 +67,21 @@ model = KNN(X, Y, 3)
 # vypsání vnitřní reprezentace
 print(model.dataset)
 
+# setřídění pole podle elementu x index 1
+# klíč je element a hodnota je počet výskytů
+pole = [ [5, 4], [8, 2], [1, 3], [12, 1] ]
+print(sorted(pole, key=lambda x: x[1]))
+
+# vrací n elementů s největším počtem výskytů
+# jako list tuplů
+pole = [ 2, 1, 5, 3, 1, 6, 8, 1, 3, 1 ]
+print(Counter(pole).most_common(2))
+
+# iris dataset
+# -------------------------------------------------------
+X, Y= load_iris(True)
+# rozdělení datasetu na cvičnou a testovací část  (80% ku 20%):
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y)
+
+model = KNN(X_train, Y_train, 5)
+print(model.score(X_test, Y_test))
